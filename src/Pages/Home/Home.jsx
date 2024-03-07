dfcfedcb18466b2729fd203a71da00adc42329b8
DogancanKaratas <karatasdgncn21@gmail.com>
2024-03-07 22:37:37 +0200

    deneme

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <main className="home-main">
       <h3>Hi! My Name Is </h3>
       <h1>Doğancan <span className="red-span">KARATAŞ</span></h1>
-      <h3>I'm a Front-End Developer</h3>
+      <h3>I'm a Front-End Developer.</h3>
     </main>
   )
 }
